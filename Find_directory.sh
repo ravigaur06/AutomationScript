da5198e739058ba0c364960e783e5c29401c0d91
Ubuntu <ubuntu@ip-172-31-63-105.ec2.internal>
2023-02-05 10:20:56 +0000

    Script to find directories in given path.

--- Find_directory.sh
+++ Find_directory.sh
@@ -1,3 +1,6 @@
-#/bin/bash
-echo "Number of Directory present in current working directory is :"
-ls -l | grep -E "^d"
+#!/bin/bash
+echo "Enter path name where you want to find number of directory : $1"
+
+echo "Number of Directory present in $1 path is :" 
+
+ls -l $1 | grep -E "^d"
