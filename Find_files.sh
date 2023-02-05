004b3eb1e6bedd41b7928270e28ad2ed2d58d89d
Ravi Gaur <ravigaur06@gmail.com>
2023-02-05 11:17:21 +0000

    Script to find number of files in given path

--- Find_files.sh
+++ Find_files.sh
@@ -1,4 +1,10 @@
-#/bin/bash
-echo "Script to find files in current directory."
-ls -l | grep "^-"
-echo "Number of files in current directory are : " | ls -l | grep "^-" | wc -l
+#!/bin/bash
+
+if [ $# -eq 0 ]
+then
+	echo "Please invoke this script with one command-line arguments."
+else
+	echo "Script to find files in path given is : $1"
+	echo "Number of files present in $1 path is :"
+	ls -l | grep "^-"
+fi
