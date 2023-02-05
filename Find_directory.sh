7780d6c636dce7e6f70c74a09013800f65cc8a27
Ubuntu <ubuntu@ip-172-31-63-105.ec2.internal>
2023-02-05 10:55:14 +0000

    Script to find number of directory in given path.

--- Find_directory.sh
+++ Find_directory.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-echo "Enter path name where you want to find number of directory : $1"
 
-echo "Number of Directory present in $1 path is :" 
-
-ls -l $1 | grep -E "^d"
+if [ $# -eq 0 ]
+then 
+	echo "Please invoke this script with one command-line arguments."
+else
+	echo "Enter path name where you want to find number of directories : $1"
+	echo "Number of Directory present in $1 path is :"
+	ls -l $1 | grep -E "^d"
+fi
